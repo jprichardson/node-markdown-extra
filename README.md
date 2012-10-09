631cfbee0bcbefa977106d617af18d0dd72efcd8
JP Richardson <jprichardson@gmail.com>
2012-10-09 17:59:48 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,29 @@
+Node.js - markdown-extra
+================
+
+Some extra methods for parsing markdown.
+
+
+Why?
+----
+
+[Potter](http://pottercms.com) needed some markdown parsing methods.
+
+
+Installation
+------------
+
+    npm install markdown-extra
+
+
+
+See tests for usage.
+
+License
+-------
+
+(MIT License)
+
+Copyright 2012, JP Richardson  <jprichardson@gmail.com>
+
+
---
+++ test/markdown-extra.test.js
@@ -0,0 +1,69 @@
+var testutil = require('testutil')
+  , mde = require('../lib/markdown-extra')
+
+var data = [""
+       , "<!--"
+       , "author: JP Richardson"
+       , "publish: 2012-03-04"
+       , "tags: war, history"
+       , "anything: can write anything"
+       , "-->"
+       , ""
+       , "The Fall of the Roman Empire"
+       , "============================"
+       , ""
+       , "**Julius Ceasar** was..."
+       , ""
+       , ""].join('\n');
+
+var data2 = [""
+       , "<!--"
+       , "author: JP Richardson"
+       , "publish: 2012-03-04"
+       , "tags: war, history"
+       , "anything: can write anything"
+       , "-->"
+       , ""
+       , "# The Fall of the Roman Empire"
+       , ""
+       , "**Julius Ceasar** was..."
+       , ""
+       , ""].join('\n');
+
+
+suite('markdown-extra')
+
+test('+ metadata', function() {
+  var metaData = mde.metadata(data);
+
+  var d = [
+      "author: JP Richardson"
+    , "publish: 2012-03-04"
+    , "tags: war, history"
+    , "anything: can write anything"
+  ].join('\n')
+
+  T (metaData === d)
+})
+
+test('+ content', function() {
+  var content = mde.content(data2);
+
+  var d = [
+      "# The Fall of the Roman Empire"
+    , ""
+    , "**Julius Ceasar** was..."
+    , ""
+    , ""].join('\n');
+
+  T (content === d);
+})
+
+test('+ title', function() {
+  var title1 = mde.title(data);
+  var title2 = mde.title(data2);
+
+  T (title1 === 'The Fall of the Roman Empire')
+  T (title2 === 'The Fall of the Roman Empire')
+})
+

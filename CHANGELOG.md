b59fcaa8d39579a0b23722f913bb88d34039c6a7
JP Richardson <jprichardson@gmail.com>
2012-10-11 01:10:16 -0500

    Added optional parser param to metadata().

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+0.0.2 / 2012-10-11
+------------------
+* Added parser option to metadata function.
+
 0.0.1 / 2012-10-09
 ------------------
 * Initial release.
--- test/markdown-extra.test.js
+++ test/markdown-extra.test.js
@@ -46,6 +46,22 @@ test('+ metadata', function() {
   T (metaData === d)
 })
 
+test('+ metadata / with parsing', function() {
+  var metadata = mde.metadata(data, function(md){ 
+    var retObj = {};
+    md.split('\n').forEach(function(line) {
+      var data = line.split(':');
+      retObj[data[0].trim()] = data[1].trim();
+    });
+    return retObj;
+  });
+
+  T (metadata.author === 'JP Richardson')
+  T (metadata.publish === '2012-03-04')
+  T (metadata.tags === 'war, history')
+  T (metadata.anything === 'can write anything')
+})
+
 test('+ content', function() {
   var content = mde.content(data2);
 
